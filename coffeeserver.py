2f9d7396b72f42fc8af700d0aeafaebe514dd43c
Mathias Garbe <me@mathias-garbe.de>
2012-01-12 19:19:16 +0100

    Moves https server to seperate file

--- coffeeserver.py
+++ coffeeserver.py
@@ -1,97 +1,17 @@
 import math, hashlib, time, random, socket, os, sys, json, cgi, ast
-from SocketServer import BaseServer
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from OpenSSL import SSL
 
 from payment import *
 
 sys.path.insert(0, "thirdparty/")
 sys.path.insert(0, "coffeeprotocol/")
 import config
+
+# Coffeeprotcol
 from coffeeprotocol import *
 
-def toInt(s):
-    i = 0
-    try:
-        i = int(s)
-    except:
-        pass
-    return i
+# HTTPS Server
+from httpsserver import *
 
-class SecureHTTPServer(HTTPServer):
-    def __init__(self, server_address, HandlerClass, payment, server_cert, client_pub, debug = False):
-        BaseServer.__init__(self, server_address, HandlerClass)
-        ctx = SSL.Context(SSL.SSLv23_METHOD)
-        self.payment = payment
-        self.debug = debug
-        #server.pem's location (containing the server private key and
-        #the server certificate).
-        fpem = server_cert
-        self.client_pub = client_pub
-        ctx.use_privatekey_file(fpem)
-        ctx.use_certificate_file(fpem)
-        self.socket = SSL.Connection(ctx, socket.socket(self.address_family, self.socket_type))
-
-        self.protocol = CoffeeProtocol()
-
-        self.server_bind()
-        self.server_activate()
-
-
-class SecureHTTPRequestHandler(SimpleHTTPRequestHandler):
-    def setup(self):
-        self.connection = self.request
-        self.rfile = socket._fileobject(self.request, "rb", self.rbufsize)
-        self.wfile = socket._fileobject(self.request, "wb", self.wbufsize)
-
-    def do_GET(self):
-        item = None
-        if self.path.startswith("/resource/item/"):
-            id = toInt(self.path[15:])
-            item = self.server.payment.getItemById(id)
-
-        if item is not None:
-            self.send_response(200)
-            self.send_header('Content-type','image/png')
-            self.end_headers()
-            file = open("resource/items/" + item.image, "r")
-            self.wfile.write(file.read())
-        else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write("Sorry.")
-        
-    def do_POST(self):
-        form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type'], })
-        request = None
-        if form['request']:
-            request = form['request'].value
-            print request
-            request = ast.literal_eval(request)
-        
-        if self.path=='/payment/':
-            self.send_response(200)
-            self.send_header('Content-type','application/json')
-            self.end_headers()
-    
-            #req = self.server.protocol.parseRequest(request, "public.pem")
-            req = self.server.protocol.parseRequest(request, self.server.client_pub)
-            if self.server.debug:
-                print "Request:"
-                print req
-            resp = self.server.payment.parseRequest(req, self.server.protocol.buildResponse())
-            resp_comp = resp.compile()
-            if self.server.debug:
-                print "Response:"
-                print resp_comp
-            self.wfile.write(resp_comp)
-        else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write("")
 
 def start():
     server_address = ('', 1443)
